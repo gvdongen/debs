5d8443c1e83f846e3b4e91b9f623154d8d8fc538
Giselle van Dongen <giselle.vd@gmail.com>
2024-06-28 12:00:20 +0200

    Update

--- java-hello-world-gradle/src/main/java/my/example/CartObject.java
+++ java-hello-world-gradle/src/main/java/my/example/CartObject.java
@@ -1,15 +1,40 @@
 package my.example;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import dev.restate.sdk.ObjectContext;
 import dev.restate.sdk.annotation.Handler;
 import dev.restate.sdk.annotation.VirtualObject;
+import dev.restate.sdk.common.StateKey;
+import dev.restate.sdk.serde.jackson.JacksonSerdes;
+
+import java.time.Duration;
+import java.util.HashSet;
+import java.util.Set;
 
 @VirtualObject
 public class CartObject {
 
+    private static final StateKey<Set<String>> CART =
+            StateKey.of("tickets",
+                    JacksonSerdes.of(new TypeReference<>() {}));
+
     @Handler
     public boolean addTicket(ObjectContext ctx, String ticketId){
-        return true;
+        boolean success = TicketObjectClient.fromContext(ctx, ticketId)
+                .reserve()
+                .await();
+
+        if (success) {
+            Set<String> cart = ctx.get(CART).orElseGet(HashSet::new);
+            cart.add(ticketId);
+            ctx.set(CART, cart);
+
+            CartObjectClient.fromContext(ctx, ctx.key())
+                    .send(Duration.ofMinutes(15))
+                    .expireTicket(ticketId);
+        }
+
+        return success;
     }
 
     @Handler
