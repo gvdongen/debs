d783817deb6bf6767dd8fa9558acd784e57e4479
Giselle van Dongen <giselle.vd@gmail.com>
2024-06-28 11:27:00 +0200

    Init

---
+++ java-hello-world-gradle/src/main/java/my/example/Greeter.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2024 - Restate Software, Inc., Restate GmbH
+ *
+ * This file is part of the Restate examples,
+ * which is released under the MIT license.
+ *
+ * You can find a copy of the license in the file LICENSE
+ * in the root directory of this repository or package or at
+ * https://github.com/restatedev/examples/
+ */
+
+package my.example;
+
+import dev.restate.sdk.Context;
+import dev.restate.sdk.annotation.Handler;
+import dev.restate.sdk.annotation.Service;
+import dev.restate.sdk.http.vertx.RestateHttpEndpointBuilder;
+
+/**
+ * Template of a Restate service and handler
+ * Have a look at the Java QuickStart to learn how to run this: https://docs.restate.dev/get_started/quickstart?sdk=java
+ */
+@Service
+public class Greeter {
+
+  @Handler
+  public String greet(Context ctx, String greeting) {
+    return greeting;
+  }
+
+  public static void main(String[] args) {
+    RestateHttpEndpointBuilder.builder()
+            .bind(new Greeter())
+            .buildAndListen();
+  }
+}
