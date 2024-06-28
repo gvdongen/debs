be684d2228b47b74bac310a0cd1c01f13e1e8aa2
Giselle van Dongen <giselle.vd@gmail.com>
2024-06-28 11:48:19 +0200

    Update readme

--- java-hello-world-gradle/src/main/java/my/example/TicketObject.java
+++ java-hello-world-gradle/src/main/java/my/example/TicketObject.java
@@ -1,24 +1,44 @@
 package my.example;
 
+import dev.restate.sdk.JsonSerdes;
 import dev.restate.sdk.ObjectContext;
 import dev.restate.sdk.annotation.Handler;
 import dev.restate.sdk.annotation.VirtualObject;
+import dev.restate.sdk.common.StateKey;
 
 @VirtualObject
 public class TicketObject {
 
+    private static final StateKey<String> STATUS =
+            StateKey.of("status", JsonSerdes.STRING);
+
     @Handler
     public boolean reserve(ObjectContext ctx){
-        return true;
+        String status = ctx.get(STATUS).orElse("Available");
+
+        if(status.equals("Available")){
+            ctx.set(STATUS, "Reserved");
+            return true;
+        }
+
+        return false;
     }
 
     @Handler
     public void unreserve(ObjectContext ctx){
+        String status = ctx.get(STATUS).orElse("Available");
+
+        if(!status.equals("Sold")){
+            ctx.clear(STATUS);
+        }
     }
 
     @Handler
     public void markAsSold(ObjectContext ctx){
+        String status = ctx.get(STATUS).orElse("Available");
 
+        if(status.equals("Reserved")){
+            ctx.set(STATUS, "Sold");
+        }
     }
-
 }
